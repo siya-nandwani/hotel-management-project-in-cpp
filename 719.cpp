e806b2736bc8f267d6f76fef4a3fd431cb89bbfc
Siya Nandwani <nandwanisiya5@gmail.com>
2026-01-15 12:40:56 +0530

    Initial commit

---
+++ 719.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"a:";
+    cin>>a;
+    a+=6;
+    cout<<a<<endl;
+
+    cout<<(a<<1)<<endl;
+    return 0;
+}
---
+++ SIYA.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"a:";
+    cin>>a;
+    cout<<"b:";
+    cin>>b;
+    c = a;
+    a = b;
+    b = c;
+    cout<<"a:"<<a;
+    cout<<"b:"<<b;
+    return 0;
+}
+
